e7c847676a67d188e5543831bca0b6d11661f063
Ronald Otieno <ronaldotieno237@gmail.com>
2026-07-30 12:16:02 +0300

    canonicalized my tailwindcss

--- src/scenes/Scene01Arrival.jsx
+++ src/scenes/Scene01Arrival.jsx
@@ -41,7 +41,7 @@ export default function Scene01Arrival() {
       />
 
       {/* Cinematic Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/30 to-black/80" />
+      <div className="absolute inset-0 bg-linear-to-b from-black/60 via-black/30 to-black/80" />
 
 
       {/* Intro */}
